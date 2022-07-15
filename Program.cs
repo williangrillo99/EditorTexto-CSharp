ac93bae5b2fb9acd6c14e437d96bb02379d4f5e2
willian-grillo <willian.grillo@autoglass.com.br>
2022-07-15 11:15:40 -0300

    cria projeto

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace EditorTexto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        static void Menu()
+        {
+            Console.Clear();
+        }
+    }
+}
